4c008a3848a093b3d61f546dc98140a73551049e
Rikk <tarikmoussa90@gmial.com>
2026-04-08 10:54:38 -0300

    fix: dois if-else dentro de uma condição do try

--- src/br/com/fiap/main/ComparaString.java
+++ src/br/com/fiap/main/ComparaString.java
@@ -11,6 +11,17 @@ public class ComparaString {
             String senha = scan.next();
 
             //Verificando se senha é idêntica.
+            if (senha.equals("P4ssw0rD")){
+                System.out.println("Teste 1: Acesso autorizado!");
+            } else {
+                System.out.println("Acesso negado!");
+            }
+            //Verificando se senha é igual.
+            if (senha.equalsIgnoreCase("P4ssw0rD")){
+                System.out.println("Teste 1: Acesso concedido!");
+            } else {
+                System.out.println("Acesso impedido!");
+            }
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
