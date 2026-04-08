eee6c1414f2dceb5fe3cae53cad7b2e824966223
Rikk <tarikmoussa90@gmial.com>
2026-04-08 11:04:06 -0300

    feat: aprendendo os tipos de return do String

--- src/br/com/fiap/main/ComparaString.java
+++ src/br/com/fiap/main/ComparaString.java
@@ -11,16 +11,16 @@ public class ComparaString {
             String senha = scan.next();
 
             //Verificando se senha é idêntica.
-            if (senha.equals("P4ssw0rD")){
+            if (!senha.equals("P4ssw0rD")){
                 System.out.println("Teste 1: Acesso autorizado!");
             } else {
-                System.out.println("Acesso negado!");
+                System.out.println("Teste 2: Acesso negado!");
             }
             //Verificando se senha é igual.
             if (senha.equalsIgnoreCase("P4ssw0rD")){
                 System.out.println("Teste 1: Acesso concedido!");
             } else {
-                System.out.println("Acesso impedido!");
+                System.out.println("Teste 2: Acesso impedido!");
             }
         } catch (Exception e) {
             System.out.println(e.getMessage());
